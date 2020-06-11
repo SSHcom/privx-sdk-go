34b5665cbbbfa916598aadd9a17cd5ec3af0e3f0
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2020-06-11 16:29:24 +0300

    Allow applications to download extender config file(s)

---
+++ api/files/client.go
@@ -0,0 +1,39 @@
+//
+// Copyright (c) 2020 SSH Communications Security Inc.
+//
+// All rights reserved.
+//
+
+package files
+
+import (
+	"github.com/SSHcom/privx-sdk-go/restapi"
+)
+
+// Client is a role-store client instance.
+type Client struct {
+	api restapi.Connector
+}
+
+// New creates a new client instance to fetch config files from PrivX
+func New(api restapi.Connector) *Client {
+	return &Client{api: api}
+}
+
+func (files Client) ConfigExtender(id string) ([]byte, error) {
+	var session struct {
+		ID string `json:"session_id"`
+	}
+
+	_, err := files.api.
+		URL("/authorizer/api/v1/extender/conf/%s", id).
+		Post("", &session)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return files.api.
+		URL("/authorizer/api/v1/extender/conf/%s/%s", id, session.ID).
+		Fetch()
+}
