a74d79345d2c2facae74c0c53418ec70419d215c
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2020-06-18 16:43:04 +0300

    add mandatory Source Rule mapping for the role

--- api/rolestore/model.go
+++ api/rolestore/model.go
@@ -21,17 +21,18 @@ type User struct {
 
 // Role contains PrivX role information.
 type Role struct {
-	ID          string   `json:"id"`
-	Name        string   `json:"name"`
-	Explicit    bool     `json:"explicit" tabulate:"@userCtx"`
-	Implicit    bool     `json:"implicit" tabulate:"@userCtx"`
-	System      bool     `json:"system"`
-	GrantType   string   `json:"grant_type"`
-	Comment     string   `json:"comment"`
-	Permissions []string `json:"permissions"`
-	Context     *Context `json:"context"`
-	MemberCount int      `json:"member_count"`
-	PublicKey   []string `json:"principal_public_key_strings,omitempty"`
+	ID          string     `json:"id"`
+	Name        string     `json:"name"`
+	Explicit    bool       `json:"explicit" tabulate:"@userCtx"`
+	Implicit    bool       `json:"implicit" tabulate:"@userCtx"`
+	System      bool       `json:"system"`
+	GrantType   string     `json:"grant_type"`
+	Comment     string     `json:"comment"`
+	SourceRule  SourceRule `json:"source_rules"`
+	Permissions []string   `json:"permissions"`
+	Context     *Context   `json:"context"`
+	MemberCount int        `json:"member_count"`
+	PublicKey   []string   `json:"principal_public_key_strings,omitempty"`
 }
 
 // RoleRef is a reference to role object
@@ -47,4 +48,21 @@ type Context struct {
 	StartTime string `json:"start_time"`
 	EndTime   string `json:"end_time"`
 	Timezone  string `json:"timezone"`
+}
+
+// SourceRule defines a mapping of role to object objects in directory
+type SourceRule struct {
+	Type    string       `json:"type"`
+	Match   string       `json:"match"`
+	Source  string       `json:"source,omitempty"`
+	Pattern string       `json:"search_string,omitempty"`
+	Rules   []SourceRule `json:"rules"`
+}
+
+// SourceRuleNone creates an empty mapping source for the role
+func SourceRuleNone() SourceRule {
+	return SourceRule{
+		Type:  "GROUP",
+		Match: "ANY",
+	}
 }
