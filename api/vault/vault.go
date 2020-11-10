0c1dbf4db833e8796cd16b7cef164c2599c96e53
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2020-11-10 12:00:49 +0200

    enable PrivX vault CRUD I/O

--- api/vault/vault.go
+++ api/vault/vault.go
@@ -9,6 +9,7 @@ package vault
 import (
 	"net/url"
 
+	"github.com/SSHcom/privx-sdk-go/api/rolestore"
 	"github.com/SSHcom/privx-sdk-go/restapi"
 )
 
@@ -35,4 +36,72 @@ func (vault *Vault) Get(name string) (bag *Bag, err error) {
 		Get(bag)
 
 	return
+}
+
+type tVaultReq struct {
+	Name       string              `json:"name,omitempty"`
+	Data       interface{}         `json:"data"`
+	AllowRead  []rolestore.RoleRef `json:"read_roles,omitempty"`
+	AllowWrite []rolestore.RoleRef `json:"write_roles,omitempty"`
+}
+
+func (vault *Vault) mkVaultReq(
+	allowReadBy []string,
+	allowWriteBy []string,
+	secret interface{},
+) tVaultReq {
+	allow := func(ids []string) []rolestore.RoleRef {
+		seq := []rolestore.RoleRef{}
+		for _, id := range ids {
+			seq = append(seq, rolestore.RoleRef{ID: id})
+		}
+		return seq
+	}
+
+	return tVaultReq{
+		Data:       secret,
+		AllowRead:  allow(allowReadBy),
+		AllowWrite: allow(allowWriteBy),
+	}
+}
+
+// Create new secret to PrivX Vault
+func (vault *Vault) Create(
+	name string,
+	allowReadBy []string,
+	allowWriteBy []string,
+	secret interface{},
+) error {
+	req := vault.mkVaultReq(allowReadBy, allowWriteBy, secret)
+	req.Name = name
+
+	_, err := vault.api.
+		URL("/vault/api/v1/secrets").
+		Put(req)
+
+	return err
+}
+
+// Update existing secret at PrivX Vault
+func (vault *Vault) Update(
+	name string,
+	allowReadBy []string,
+	allowWriteBy []string,
+	secret interface{},
+) error {
+	req := vault.mkVaultReq(allowReadBy, allowWriteBy, secret)
+
+	_, err := vault.api.
+		URL("/vault/api/v1/secrets/%s", name).
+		Put(req)
+
+	return err
+}
+
+// Remove existing secret from PrivX vault
+func (vault *Vault) Remove(name string) error {
+	_, err := vault.api.
+		URL("/vault/api/v1/secrets/%s", name).
+		Delete()
+	return err
 }
