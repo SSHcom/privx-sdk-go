25d8f6c681f92fc61147e05ea5782e73fd6339d1
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2020-11-30 21:34:50 +0200

    consolidate download functions

--- api/config/client.go
+++ api/config/client.go
@@ -26,27 +26,19 @@ func New(api restapi.Connector) *ConfFileStore {
 
 // ConfigExtender fetches configuration file
 func (store *ConfFileStore) ConfigExtender(id string) ([]byte, error) {
-	var session tSession
-
-	_, err := store.api.
-		URL("/authorizer/api/v1/extender/conf/%s", id).
-		Post("", &session)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return store.api.
-		URL("/authorizer/api/v1/extender/conf/%s/%s", id, session.ID).
-		Fetch()
+	return store.config("extender/conf", id)
 }
 
 // ConfigDeploy fetches deployment script
 func (store *ConfFileStore) ConfigDeploy(id string) ([]byte, error) {
+	return store.config("deploy", id)
+}
+
+func (store *ConfFileStore) config(config, id string) ([]byte, error) {
 	var session tSession
 
 	_, err := store.api.
-		URL("/authorizer/api/v1/deploy/%s", id).
+		URL("/authorizer/api/v1/%s/%s", config, id).
 		Post("", &session)
 
 	if err != nil {
@@ -54,6 +46,6 @@ func (store *ConfFileStore) ConfigDeploy(id string) ([]byte, error) {
 	}
 
 	return store.api.
-		URL("/authorizer/api/v1/deploy/%s/%s", id, session.ID).
+		URL("/authorizer/api/v1/%s/%s/%s", config, id, session.ID).
 		Fetch()
 }
