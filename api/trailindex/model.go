2b7f6da638c7fb58ddc308ab9ccce4239ae04c58
Ilja Smelich <ilja.smelich@ssh.com>
2021-04-26 15:30:38 +0300

    fix(API): rename struct, fix lint error

--- api/trailindex/model.go
+++ api/trailindex/model.go
@@ -13,15 +13,15 @@ type Params struct {
 	Sortdir string `json:"sortdir,omitempty"`
 }
 
-// TrailIndexObject trail index response definition
-type TrailIndexObject struct {
+// IndexResponse trail index response definition
+type IndexResponse struct {
 	ConnID      string `json:"connection_id,omitempty"`
 	ChanID      string `json:"channel_id,omitempty"`
 	Protocol    string `json:"protocol,omitempty"`
 	ChannelType string `json:"type,omitempty"`
 	TimeStamp   string `json:"timestamp,omitempty"`
-	Position    int    `json:"position,omitempty"`
 	Content     string `json:"content,omitempty"`
+	Position    int    `json:"position,omitempty"`
 }
 
 // SearchRequestObject search request object definition
@@ -32,8 +32,8 @@ type SearchRequestObject struct {
 	Keywords  string `json:"keywords"`
 	StartTime string `json:"start_time"`
 	EndTime   string `json:"end_time"`
-	StartPos  *int   `json:"start_position"`
-	EndPos    *int   `json:"end_position"`
+	StartPos  int    `json:"start_position"`
+	EndPos    int    `json:"end_position"`
 }
 
 // Connection connection definition
