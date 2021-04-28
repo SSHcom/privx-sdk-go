9199a76586ebc3114a894975a7dd69913e64e2b3
Ilja Smelich <ilja.smelich@ssh.com>
2021-04-28 16:17:14 +0300

    feat(API): add new endpoints to workflow

--- api/workflow/client.go
+++ api/workflow/client.go
@@ -7,6 +7,7 @@
 package workflow
 
 import (
+	"encoding/json"
 	"net/url"
 
 	"github.com/SSHcom/privx-sdk-go/restapi"
@@ -22,10 +23,112 @@ type workflowsResult struct {
 	Items []Workflow `json:"items"`
 }
 
+type requestsResult struct {
+	Count int       `json:"count"`
+	Items []Request `json:"items"`
+}
+
 // New creates a new workflow client instance, using the
 // argument SDK API client.
 func New(api restapi.Connector) *Engine {
 	return &Engine{api: api}
+}
+
+// SetSettings store microservice settings
+func (store *Engine) SetSettings(settings *json.RawMessage) error {
+	_, err := store.api.
+		URL("/workflow-engine/api/v1/settings").
+		Put(settings)
+
+	return err
+}
+
+// Settings get settings for the microservice
+func (store *Engine) Settings() (settings *json.RawMessage, err error) {
+	_, err = store.api.
+		URL("/workflow-engine/api/v1/settings").
+		Get(&settings)
+
+	return
+}
+
+// SearchRequests search access requests
+func (store *Engine) SearchRequests(
+	offset, limit, sortdir, sortkey, filter string, searchObject *Search) ([]Request, error) {
+	result := requestsResult{}
+	filters := Params{
+		Offset:  offset,
+		Limit:   limit,
+		Sortkey: sortkey,
+		Sortdir: sortdir,
+		Filter:  filter,
+	}
+
+	_, err := store.api.
+		URL("/workflow-engine/api/v1/requests/search").
+		Query(&filters).
+		Post(&searchObject, &result)
+
+	return result.Items, err
+}
+
+// UserRequestDecision update a request in queue
+func (store *Engine) UserRequestDecision(requestID string, request Decision) error {
+	_, err := store.api.
+		URL("/workflow-engine/api/v1/requests/%s/decision", url.PathEscape(requestID)).
+		Post(&request)
+
+	return err
+}
+
+// DeleteRequest delete request item by ID.
+func (store *Engine) DeleteRequest(requestID string) error {
+	_, err := store.api.
+		URL("/workflow-engine/api/v1/requests/%s", requestID).
+		Delete()
+
+	return err
+}
+
+// Request return a request object by ID.
+func (store *Engine) Request(requestID string) (request *Request, err error) {
+	request = new(Request)
+
+	_, err = store.api.
+		URL("/workflow-engine/api/v1/requests/%s", url.PathEscape(requestID)).
+		Get(request)
+
+	return request, err
+}
+
+// CreateRequest add a workflow to the request queue.
+func (store *Engine) CreateRequest(request *Request) (string, error) {
+	var id struct {
+		ID string `json:"id"`
+	}
+
+	_, err := store.api.
+		URL("/workflow-engine/api/v1/requests").
+		Post(&request, &id)
+
+	return id.ID, err
+}
+
+// Requests get the request queue for the user
+func (store *Engine) Requests(offset, limit, filter string) ([]Request, error) {
+	result := requestsResult{}
+	filters := Params{
+		Offset: offset,
+		Limit:  limit,
+		Filter: filter,
+	}
+
+	_, err := store.api.
+		URL("/workflow-engine/api/v1/requests").
+		Query(&filters).
+		Get(&result)
+
+	return result.Items, err
 }
 
 // UpdateWorkflow update  a workflow
--- api/workflow/model.go
+++ api/workflow/model.go
@@ -65,3 +65,59 @@ type Workflow struct {
 	Requester            User   `json:"requester,omitempty"`
 	RequestedRole        Role   `json:"requested_role,omitempty"`
 }
+
+// Search request search definition
+type Search struct {
+	Keywords  string `json:"keywords,omitempty"`
+	StartTime string `json:"start_time,omitempty"`
+	EndTime   string `json:"end_time,omitempty"`
+}
+
+// Decision request decision definition
+type Decision struct {
+	Step     int    `json:"step"`
+	Decision string `json:"decision"`
+	Comment  string `json:"comment,omitempty"`
+}
+
+// RequestStepApprover request step approver definition
+type RequestStepApprover struct {
+	ID           string `json:"id,omitempty"`
+	Decision     string `json:"decision,omitempty"`
+	DecisionTime string `json:"decision_time,omitempty"`
+	Comment      string `json:"comment,omitempty"`
+	User         User   `json:"user,omitempty"`
+	Role         Role   `json:"role,omitempty"`
+}
+
+// RequestStep request step definition
+type RequestStep struct {
+	ID        string                `json:"id,omitempty"`
+	Name      string                `json:"name,omitempty"`
+	Match     string                `json:"match,omitempty"`
+	Approvers []RequestStepApprover `json:"approvers,omitempty"`
+}
+
+// Request request response definition
+type Request struct {
+	ID                   string        `json:"id,omitempty"`
+	Author               string        `json:"author,omitempty"`
+	Created              string        `json:"created,omitempty"`
+	Updated              string        `json:"updated,omitempty"`
+	UpdatedBy            string        `json:"updated_by,omitempty"`
+	Name                 string        `json:"name,omitempty"`
+	RequestJustification string        `json:"request_justification,omitempty"`
+	GrantType            string        `json:"grant_type,omitempty"`
+	GrantStart           string        `json:"grant_start,omitempty"`
+	GrantEnd             string        `json:"grant_end,omitempty"`
+	Action               string        `json:"action,omitempty"`
+	Status               string        `json:"status,omitempty"`
+	Comment              string        `json:"comment,omitempty"`
+	WorkflowID           string        `json:"workflow,omitempty"`
+	FloatingLength       int           `json:"floating_length,omitempty"`
+	TargetRoles          []string      `json:"target_roles,omitempty"`
+	Steps                []RequestStep `json:"steps,omitempty"`
+	TargetUser           User          `json:"target_user,omitempty"`
+	Requester            User          `json:"requester,omitempty"`
+	RequestedRole        Role          `json:"requested_role,omitempty"`
+}
