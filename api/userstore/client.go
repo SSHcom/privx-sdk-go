4bad56e16768446ccb385e234dba5f1234383a62
Ilja Smelich <ilja.smelich@ssh.com>
2021-05-18 08:30:45 +0300

    fix(API): refactor userstore client and model

--- api/userstore/client.go
+++ api/userstore/client.go
@@ -38,79 +38,8 @@ func New(api restapi.Connector) *UserStore {
 	return &UserStore{api: api}
 }
 
-// LocalUserTags returns local user tags
-func (store *UserStore) LocalUserTags(offset, limit, sortdir, query string) ([]string, error) {
-	result := tagsResult{}
-	filters := FilterUser{
-		Params: Params{
-			Offset:  offset,
-			Limit:   limit,
-			Sortdir: sortdir,
-			Query:   query,
-		},
-	}
-
-	_, err := store.api.
-		URL("/local-user-store/api/v1/users/tags").
-		Query(&filters).
-		Get(&result)
-
-	return result.Items, err
-}
-
-// UpdateLocalUserPassword update existing local user password
-func (store *UserStore) UpdateLocalUserPassword(id string, password *Password) error {
-	_, err := store.api.
-		URL("/local-user-store/api/v1/users/%s/password", url.PathEscape(id)).
-		Put(password)
-
-	return err
-}
-
-// UpdateLocalUser update existing local user
-func (store *UserStore) UpdateLocalUser(id string, localUser *LocalUser) error {
-	_, err := store.api.
-		URL("/local-user-store/api/v1/users/%s", url.PathEscape(id)).
-		Put(localUser)
-
-	return err
-}
-
-// DeleteLocalUser delete a local user
-func (store *UserStore) DeleteLocalUser(id string) error {
-	_, err := store.api.
-		URL("/local-user-store/api/v1/users/%s", id).
-		Delete()
-
-	return err
-}
-
-// LocalUser returns details about the local user
-func (store *UserStore) LocalUser(id string) (user *LocalUser, err error) {
-	user = new(LocalUser)
-
-	_, err = store.api.
-		URL("/local-user-store/api/v1/users/%s", url.PathEscape(id)).
-		Get(user)
-
-	return user, err
-}
-
-// CreateLocalUser create a new local PrivX user
-func (store *UserStore) CreateLocalUser(newUser LocalUser) (string, error) {
-	var id struct {
-		ID string `json:"id"`
-	}
-
-	_, err := store.api.
-		URL("/local-user-store/api/v1/users").
-		Post(newUser, &id)
-
-	return id.ID, err
-}
-
 // LocalUsers returns user details from all known local users
-func (store *UserStore) LocalUsers(offset, limit, userID, username string) ([]LocalUser, error) {
+func (store *UserStore) LocalUsers(offset, limit int, userID, username string) ([]LocalUser, error) {
 	result := usersResult{}
 	filters := FilterUser{
 		Params: Params{
@@ -129,38 +58,109 @@ func (store *UserStore) LocalUsers(offset, limit, userID, username string) ([]Lo
 	return result.Items, err
 }
 
-// CreateTrustedClient registers new client to PrivX
-func (store *UserStore) CreateTrustedClient(client TrustedClient) (string, error) {
-	var id struct {
+// CreateLocalUser create a new local PrivX user
+func (store *UserStore) CreateLocalUser(newUser LocalUser) (string, error) {
+	var object struct {
 		ID string `json:"id"`
 	}
 
 	_, err := store.api.
-		URL("/local-user-store/api/v1/trusted-clients").
-		Post(client, &id)
+		URL("/local-user-store/api/v1/users").
+		Post(newUser, &object)
 
-	return id.ID, err
+	return object.ID, err
+}
+
+// LocalUser returns details about the local user
+func (store *UserStore) LocalUser(userID string) (*LocalUser, error) {
+	user := &LocalUser{}
+
+	_, err := store.api.
+		URL("/local-user-store/api/v1/users/%s", url.PathEscape(userID)).
+		Get(user)
+
+	return user, err
+}
+
+// UpdateLocalUser update existing local user
+func (store *UserStore) UpdateLocalUser(userID string, localUser *LocalUser) error {
+	_, err := store.api.
+		URL("/local-user-store/api/v1/users/%s", url.PathEscape(userID)).
+		Put(localUser)
+
+	return err
+}
+
+// DeleteLocalUser delete a local user
+func (store *UserStore) DeleteLocalUser(userID string) error {
+	_, err := store.api.
+		URL("/local-user-store/api/v1/users/%s", userID).
+		Delete()
+
+	return err
+}
+
+// UpdateLocalUserPassword update existing local user password
+func (store *UserStore) UpdateLocalUserPassword(userID string, password *Password) error {
+	_, err := store.api.
+		URL("/local-user-store/api/v1/users/%s/password", url.PathEscape(userID)).
+		Put(password)
+
+	return err
+}
+
+// LocalUserTags returns local user tags
+func (store *UserStore) LocalUserTags(offset, limit int, sortdir, query string) ([]string, error) {
+	result := tagsResult{}
+	filters := FilterUser{
+		Params: Params{
+			Offset:  offset,
+			Limit:   limit,
+			Sortdir: sortdir,
+			Query:   query,
+		},
+	}
+
+	_, err := store.api.
+		URL("/local-user-store/api/v1/users/tags").
+		Query(&filters).
+		Get(&result)
+
+	return result.Items, err
 }
 
 // TrustedClients fetches all known trusted clients
 func (store *UserStore) TrustedClients() ([]TrustedClient, error) {
-	var seq struct {
+	var object struct {
 		Items []TrustedClient
 	}
 
 	_, err := store.api.
 		URL("/local-user-store/api/v1/trusted-clients").
-		Get(&seq)
+		Get(&object)
 
-	return seq.Items, err
+	return object.Items, err
+}
+
+// CreateTrustedClient registers new client to PrivX
+func (store *UserStore) CreateTrustedClient(client TrustedClient) (string, error) {
+	var object struct {
+		ID string `json:"id"`
+	}
+
+	_, err := store.api.
+		URL("/local-user-store/api/v1/trusted-clients").
+		Post(client, &object)
+
+	return object.ID, err
 }
 
 // TrustedClient returns details about the client
-func (store *UserStore) TrustedClient(id string) (*TrustedClient, error) {
-	client := new(TrustedClient)
+func (store *UserStore) TrustedClient(clientID string) (*TrustedClient, error) {
+	client := &TrustedClient{}
 
 	_, err := store.api.
-		URL("/local-user-store/api/v1/trusted-clients/%s", id).
+		URL("/local-user-store/api/v1/trusted-clients/%s", clientID).
 		Get(client)
 
 	if err != nil {
@@ -170,20 +170,20 @@ func (store *UserStore) TrustedClient(id string) (*TrustedClient, error) {
 	return client, nil
 }
 
-// UpdateTrustedClient update existing trusted client
-func (store *UserStore) UpdateTrustedClient(id string, client *TrustedClient) error {
+// DeleteTrustedClient removes the client
+func (store *UserStore) DeleteTrustedClient(clientID string) error {
 	_, err := store.api.
-		URL("/local-user-store/api/v1/trusted-clients/%s", url.PathEscape(id)).
-		Put(client)
+		URL("/local-user-store/api/v1/trusted-clients/%s", clientID).
+		Delete()
 
 	return err
 }
 
-// DeleteTrustedClient removes the client
-func (store *UserStore) DeleteTrustedClient(id string) error {
+// UpdateTrustedClient update existing trusted client
+func (store *UserStore) UpdateTrustedClient(clientID string, client *TrustedClient) error {
 	_, err := store.api.
-		URL("/local-user-store/api/v1/trusted-clients/%s", id).
-		Delete()
+		URL("/local-user-store/api/v1/trusted-clients/%s", url.PathEscape(clientID)).
+		Put(client)
 
 	return err
 }
@@ -201,35 +201,20 @@ func (store *UserStore) ExtenderClients() ([]TrustedClient, error) {
 
 // APIClients returns list of all registered api clients
 func (store *UserStore) APIClients() ([]APIClient, error) {
-	var seq struct {
+	var object struct {
 		Items []APIClient
 	}
 
 	_, err := store.api.
 		URL("/local-user-store/api/v1/api-clients").
-		Get(&seq)
+		Get(&object)
 
-	return seq.Items, err
-}
-
-// APIClient returns details about API client
-func (store *UserStore) APIClient(id string) (*APIClient, error) {
-	client := new(APIClient)
-
-	_, err := store.api.
-		URL("/local-user-store/api/v1/api-clients/%s", id).
-		Get(client)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
+	return object.Items, err
 }
 
 // CreateAPIClient creates new API client
 func (store *UserStore) CreateAPIClient(name string, roles []string) (string, error) {
-	var id struct {
+	var object struct {
 		ID string `json:"id"`
 	}
 
@@ -244,25 +229,40 @@ func (store *UserStore) CreateAPIClient(name string, roles []string) (string, er
 
 	_, err := store.api.
 		URL("/local-user-store/api/v1/api-clients").
-		Post(req, &id)
+		Post(req, &object)
 
-	return id.ID, err
+	return object.ID, err
 }
 
-// UpdateAPIClient update existing api client
-func (store *UserStore) UpdateAPIClient(id string, client *APIClient) error {
+// APIClient returns details about API client
+func (store *UserStore) APIClient(clientID string) (*APIClient, error) {
+	client := &APIClient{}
+
 	_, err := store.api.
-		URL("/local-user-store/api/v1/api-clients/%s", url.PathEscape(id)).
-		Put(client)
+		URL("/local-user-store/api/v1/api-clients/%s", clientID).
+		Get(client)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// DeleteAPIClient removes existing API client
+func (store *UserStore) DeleteAPIClient(clientID string) error {
+	_, err := store.api.
+		URL("/local-user-store/api/v1/api-clients/%s", clientID).
+		Delete()
 
 	return err
 }
 
-// DeleteAPIClient removes existing API client
-func (store *UserStore) DeleteAPIClient(id string) error {
+// UpdateAPIClient update existing api client
+func (store *UserStore) UpdateAPIClient(clientID string, client *APIClient) error {
 	_, err := store.api.
-		URL("/local-user-store/api/v1/api-clients/%s", id).
-		Delete()
+		URL("/local-user-store/api/v1/api-clients/%s", url.PathEscape(clientID)).
+		Put(client)
 
 	return err
 }
--- api/userstore/model.go
+++ api/userstore/model.go
@@ -19,8 +19,8 @@ const (
 
 // Params struct for pagination queries.
 type Params struct {
-	Offset  string `json:"offset,omitempty"`
-	Limit   string `json:"limit,omitempty"`
+	Offset  int    `json:"offset,omitempty"`
+	Limit   int    `json:"limit,omitempty"`
 	Sortdir string `json:"sortdir,omitempty"`
 	Query   string `json:"query,omitempty"`
 }
