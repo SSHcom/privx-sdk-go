3d45948f9b5b44ef3ac9f4fda4d7179ff50ea20f
Ilja Smelich <ilja.smelich@ssh.com>
2021-05-20 13:10:32 +0300

    feat(API): add auth client to sdk

---
+++ api/auth/model.go
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2021 SSH Communications Security Inc.
+//
+// All rights reserved.
+//
+
+package auth
+
+import "time"
+
+// KeyValue key value definition
+type KeyValue struct {
+	Key   string `json:"k"`
+	Value string `json:"v"`
+}
+
+// ServiceStatus auth service status definition
+type ServiceStatus struct {
+	Variant       string     `json:"variant,omitempty"`
+	Version       string     `json:"version,omitempty"`
+	ApiVersion    string     `json:"api_version,omitempty"`
+	Status        string     `json:"status,omitempty"`
+	StatusMessage string     `json:"status_message,omitempty"`
+	ApplicationID string     `json:"app_id,omitempty"`
+	ServerMode    string     `json:"server-mode,omitempty"`
+	StatusDetails []KeyValue `json:"status_details,omitempty"`
+	StartTime     time.Time  `json:"start_time,omitempty"`
+}
