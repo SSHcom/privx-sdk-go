fba059815ff4a639693198a960135ceb6be57199
iljaSL <ismelich.tech@protonmail.com>
2024-08-15 13:18:02 +0300

    fix: add missing fields to structs

--- api/secretsmanager/model.go
+++ api/secretsmanager/model.go
@@ -51,9 +51,10 @@ type CompileScriptRequest struct {
 type TargetDomain struct {
 	ID                   string                 `json:"id"`
 	Name                 string                 `json:"name"`
+	DomainName           string                 `json:"domain_name"`
 	Enabled              bool                   `json:"enabled"`
 	PeriodicScan         bool                   `json:"periodic_scan"`
-	PeriodicScanInterval int                    `json:"periodic_scan_interval,omitempty"`
+	PeriodicScanInterval int                    `json:"periodic_scan_interval"`
 	ScanStatus           string                 `json:"scan_status,omitempty"`
 	ScanMessage          string                 `json:"scan_message,omitempty"`
 	LastScanned          *time.Time             `json:"last_scanned,omitempty"`
@@ -152,6 +153,7 @@ type ManagedAccount struct {
 	Username           string                `json:"username"`
 	Email              string                `json:"email,omitempty"`
 	FullName           string                `json:"full_name,omitempty"`
+	SamAccountName     string                `json:"sam_account_name,omitempty"`
 	SourceID           string                `json:"source_id,omitempty"`
 	SecurityID         string                `json:"security_id,omitempty"`
 	AdditionalData     map[string]string     `json:"additional_data,omitempty"`
